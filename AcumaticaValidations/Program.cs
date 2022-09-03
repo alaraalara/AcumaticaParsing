d269f9680c5b725a024d944e5d9d09145b5a2017
alaraalara <alaraozkutucu@gmail.com>
2022-09-02 21:18:35 -0400

    Add project files.

---
+++ AcumaticaValidations/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcumaticaValidations
+//note: add a helper to create a connection strign with specific format
+//add function to create a table if it doesnt exists.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequestEntities validations = new WebRequestEntities("name=Validations");
+           
+            Log odataLogs;
+
+            WebRequestEntities context = new WebRequestEntities("name=AcuProx");
+            Log log;
+            using (context)
+            {
+            
+                Console.WriteLine(context.Database.Exists());
+                Console.WriteLine(validations.Database.Exists());
+
+                
+                var odata = context.Logs.SqlQuery("Select * from Log where Path like '%/odata/%'").ToList<Log>();
+                foreach (Log logOdata in odata)
+                {
+                   
+                    validations.Logs.Add(logOdata);
+                    
+                    validations.SaveChanges();
+                }
+                //check if it has been added!
+                foreach(var validatedData in validations.Logs)
+                {
+                    Console.WriteLine(validatedData.Headers);
+                }
+
+               
+
+            }
+
+            
+
+            }
+
+    
+        }
+    }
+
